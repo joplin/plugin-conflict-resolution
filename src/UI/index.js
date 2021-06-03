f06a9ccec812383ffdb225216897b6b158158a64
Ahmed <ahmad.gasser@gmail.com>
2021-06-03 23:32:19 +0200

    WIP: Added base dialog

---
+++ src/UI/index.js
@@ -0,0 +1,1 @@
+var myCodeMirror = CodeMirror(document.getElementById('conflictRes-Editor'));
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import joplin from 'api';
 
 joplin.plugins.register({
 	onStart: async function() {
+		const dialogs = joplin.views.dialogs;
+		
+		// DEBUG
+		const handle = await dialogs.create("conflictRes-mainDialog");
+		await dialogs.setHtml(handle, `<div id="conflictRes-Editor"></div>`);
+		await dialogs.addScript(handle, './UI/index.css');
+		await dialogs.addScript(handle, './UI/index.js');
+		const result = await dialogs.open(handle);
+		console.dir(result);
+
 		console.info('Conflict Resolution Plugin loaded!');
 	},
 });
