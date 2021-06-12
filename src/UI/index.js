828c2648c5c3f15bd7cdd00b9454ae29b461e797
Ahmed <ahmad.alwasifi@gmail.com>
2021-06-12 19:46:41 +0200

    Fixed bug where markdown addon isn't loaded in some cases.

--- src/UI/index.js
+++ src/UI/index.js
@@ -7,7 +7,7 @@ function log(message) {
     console.log(`Conflict Resolution Plugin: ` + message);
 }
 
-function initCodeMirror() {
+async function initCodeMirror() {
     log('Initing codemirror instance...');
 
     // These scripts have to be loaded here in order to ensure Codemirror.js is already loaded by now.
@@ -16,19 +16,33 @@ function initCodeMirror() {
     script.src = installPath + '/UI/codemirror/mode/markdown/markdown.js';
     script2.src = installPath + '/UI/codemirror/addon/merge/merge.js';
 
-    script2.onload = function () {
-        myCodeMirror = CodeMirror.MergeView(document.getElementById('conflictRes-Editor'), {
-            origLeft: origNote,
-            mode: 'markdown',
-            lineNumbers: true,
-            connect: 'align',
-            value: curNote,
-            lineWrapping: true
-        });
-    }
+    // This is needed because I have to wait for both the scripts to load before I do anything.
+    let promises = [
+        new Promise((res) => {
+            script.onload = function () {
+                res();
+            }
+        }),
+        new Promise((res) => {
+            script2.onload = function () {
+                res();
+            }
+        })
+    ];
 
     document.head.appendChild(script);
     document.head.appendChild(script2);
+
+    await Promise.all(promises);
+    
+    myCodeMirror = CodeMirror.MergeView(document.getElementById('conflictRes-Editor'), {
+        origLeft: origNote,
+        mode: 'markdown',
+        lineNumbers: true,
+        connect: 'align',
+        value: curNote,
+        lineWrapping: true
+    });
 }
 
 // TODO: find a better way to wait for document load.
