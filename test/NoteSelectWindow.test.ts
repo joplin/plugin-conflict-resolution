9c642f9df57fd67566df122337b95832786d8d18
Ahmed <ahmad.alwasifi@gmail.com>
2021-06-25 01:02:33 +0200

    some efforts in trying testing

--- test/NoteSelectWindow.test.ts
+++ test/NoteSelectWindow.test.ts
@@ -1,13 +1,21 @@
 import JoplinData from "api/JoplinData";
-import JoplinViewsDialogs from "api/JoplinViewsDialogs";
 import { NoteSelectWindow } from "src/NoteSelectWindow";
 
-jest.mock("./api/JoplinViewsDialogs");
+jest.mock("./../api/JoplinViewsDialogs.d.ts");
+const JoplinViewsDialogs = require("./../api/JoplinViewsDialogs.d.ts");
 
-beforeEach(() => {
-    JoplinViewsDialogs.mockClear();
-});
+/*JoplinViewsDialogs.mockImplementation(() => {
+    return {
+        create: jest.fn(),
+        showMessageBox: jest.fn(),
+        setHtml: jest.fn(),
+        addScript: jest.fn(),
+        setButtons: jest.fn(),
+        open: jest.fn()
+    };
+});*/
 
 test("Should open a select window", () => {
-    
+    let dialogsApi = new JoplinViewsDialogs();
+    //let wind = new NoteSelectWindow(dialogsApi, JoplinData, fs, "");
 });
