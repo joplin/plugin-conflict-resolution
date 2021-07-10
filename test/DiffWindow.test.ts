12a7dfe30a85c961060281adcbed87c5dc09f16e
Ahmad Mamdouh <ahmad.alwasifi@gmail.com>
2021-07-11 01:21:49 +0200

    Added tests for the DiffWindow. (#8)
    * Added tests for the DiffWindow.

---
+++ test/DiffWindow.test.ts
@@ -0,0 +1,104 @@
+const JoplinViewsDialogs = require('./../api/JoplinViewsDialogs.d.ts');
+const JoplinData = require('./../api/JoplinData.d.ts');
+import {DiffWindow} from '../src/DiffWindow';
+
+/* JOPLIN API MOCK */
+const mockedDialogApi = {
+    create: jest.fn(),
+    addScript: jest.fn(),
+    setHtml: jest.fn(),
+    open: jest.fn(),
+    setButtons: jest.fn(),
+};
+jest.mock('./../api/JoplinViewsDialogs.d.ts', () => {
+    return jest.fn().mockImplementation(() => {
+        return {
+            create: mockedDialogApi.create,
+            addScript: mockedDialogApi.addScript,
+            setHtml: mockedDialogApi.setHtml,
+            open: mockedDialogApi.open,
+            setButtons: mockedDialogApi.setButtons,
+        };
+    });
+});
+
+const mockedDataApi = {
+    get: jest.fn(),
+};
+jest.mock('./../api/JoplinData.d.ts', () => {
+    return jest.fn().mockImplementation(() => {
+        return {
+            get: mockedDataApi.get,
+        };
+    });
+});
+
+const fs = require('fs');
+jest.mock('fs');
+
+describe('DiffWindow', () => {
+    const mockedJoplinDialogs = new JoplinViewsDialogs();
+    const mockedJoplinData = new JoplinData();
+
+    mockedDialogApi.create.mockResolvedValue('test-dialog-id');
+    fs.readFile.mockResolvedValue('<form></form>');
+
+    mockedDialogApi.open.mockResolvedValue({
+        id: 'submit',
+        formData: {
+            note: {
+                newNoteContents: 'Final Contents',
+                newNoteTitle: 'Final Title',
+            },
+        },
+    });
+    mockedDataApi.get.mockImplementation((arr, obj) => {
+        if (arr[0] === 'notes') {
+            if (arr[1] === '8c0d6c64f37f409ba4edf517bab861b5') {
+                return {
+                    title: 'Test Title',
+                    body: 'Test Content',
+                };
+            } else if (arr[1] === '673f0783febc4ead8187727781ab3b32') {
+                return {
+                    title: 'Test Title 2',
+                    body: 'Test Content 2',
+                };
+            }
+        }
+    });
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('check having correct html and that all inputs have correct values', async () => {
+        const dlg = new DiffWindow(mockedJoplinDialogs, mockedJoplinData, fs, 'D:');
+        dlg.init('abc123');
+
+        await dlg.openWindow('8c0d6c64f37f409ba4edf517bab861b5', '673f0783febc4ead8187727781ab3b32');
+
+        // This also checks that all the fields are set correctly, including titles, note contents and the html from index.html.
+        expect(mockedDialogApi.setHtml.mock.calls[0]).toMatchSnapshot('dialogHtml');
+    });
+
+    it('should return final content and title on pressing ok', async () => {
+        const dlg = new DiffWindow(mockedJoplinDialogs, mockedJoplinData, fs, 'D:');
+        dlg.init('abc123');
+        const res = await dlg.openWindow('8c0d6c64f37f409ba4edf517bab861b5', '673f0783febc4ead8187727781ab3b32');
+        expect(res).toStrictEqual({
+            NoteContents: 'Final Contents',
+            NoteTitle: 'Final Title',
+        });
+    });
+
+    it('should return null when pressing cancel', async () => {
+        const dlg = new DiffWindow(mockedJoplinDialogs, mockedJoplinData, fs, 'D:');
+        dlg.init('abc123');
+        mockedDialogApi.open.mockResolvedValueOnce({
+            id: 'cancel',
+        });
+        const res = await dlg.openWindow('8c0d6c64f37f409ba4edf517bab861b5', '673f0783febc4ead8187727781ab3b32');
+        expect(res).toBe(null);
+    });
+});
